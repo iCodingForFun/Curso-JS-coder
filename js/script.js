0efbc3e267ee359533b6570e9ea5da5ea5f4db36
Victor Mosqueda <vdmc_123@hotmail.com>
2021-06-28 15:56:11 -0300

    Curso Javascript

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// Desafío #1 
+
+//Datos de entrada
+ var nombre = prompt("Por favor, ingrese su nombre");
+ var apellido = prompt("Por favor, ingrese su apellido");
+
+// //Datos de salida
+ var nombreCompleto = nombre + " " + apellido;
+
+ alert ("¡Gracias! " + nombreCompleto+". Tus datos se han guardado correctamente");
+
+
+// Desafío #2 
+//Pide la edad y si es mayor de 18 años indica que ya puede conducir.
+
+// let edad = prompt("Ingrese su edad")
+
+// if (Number(edad)){
+//     if (edad <= 17){
+//         alert ("Eres menor de edad para conducir")
+//     }
+//     else if (edad >= 18){
+//         alert ("Eres mayor de edad, ya puedes conducir")
+//     }
+// }
+// else{
+//     alert("Ingrese un numero valido")
+// }
