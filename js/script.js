319031fdf567e01454e2aad318a55e9c1e70e385
Victor Mosqueda <vdmc_123@hotmail.com>
2021-06-29 16:45:04 -0300

    Cambios

--- js/script.js
+++ js/script.js
@@ -1,13 +1,46 @@
+// Desafío # 4
+//FUNCION CON PARAMETROS Y RETORNO DE RESULTADO
+function sumarDosNumeros(num1, num2) {
+    var montoTotal = num1 + num2;
+    return montoTotal;
+}
+
+var precioProd1 = 1000;
+var precioProd2 = 890;
+
+var gastoTotal = sumarDosNumeros(precioProd1, precioProd2);
+var descuento = gastoTotal * 0.15;
+var precioConDesc = gastoTotal - descuento;
+console.log("Gastaste $:" + gastoTotal);
+console.log("El descuento es de $:" + descuento);
+console.log("Precio final con descuento $" + precioConDesc);
+
+
+// Desafío # 3
+//''Promediador de notas''
+// let acumuladorNotas = 0;
+// let promedio;
+// let cantNotas = parseInt(prompt("Que cantidad de notas vas a promediar?"));
+// let nota;
+
+// for (let i = 1; i <= cantNotas; i++) {
+//     nota = parseInt(prompt("Ingrese la nota " + i));
+//     acumuladorNotas = +nota;
+// }
+
+// promedio = acumuladorNotas / cantNotas;
+// alert("El promedio de tus notas es: " + promedio);
+
 // Desafío #1 
 
 //Datos de entrada
- var nombre = prompt("Por favor, ingrese su nombre");
- var apellido = prompt("Por favor, ingrese su apellido");
+//  var nombre = prompt("Por favor, ingrese su nombre");
+//  var apellido = prompt("Por favor, ingrese su apellido");
 
 // //Datos de salida
- var nombreCompleto = nombre + " " + apellido;
+//  var nombreCompleto = nombre + " " + apellido;
 
- alert ("¡Gracias! " + nombreCompleto+". Tus datos se han guardado correctamente");
+//  alert ("¡Gracias! " + nombreCompleto+". Tus datos se han guardado correctamente");
 
 
 // Desafío #2 
